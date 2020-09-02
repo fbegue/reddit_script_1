2a9b8ee666ce2277acbe142cf21e867501c3ac92
fbegue <eugene.f.begue@gmail.com>
2020-09-02 09:48:47 -0400

    comments

--- script_1.js
+++ script_1.js
@@ -1,6 +1,89 @@
 var rp = require('request-promise');
 const fs = require('fs');
-var token = "52995552-07gMazDevEhpZluTGWodcSGJ8xg";
+var _ = require('lodash');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+	var asdf = _.uniqBy([],'created_utc')
+	console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+var comments = [];
+
+app.get('/', (req, res) => {
+
+	Promise.resolve()
+		.then(
+			function() {
+				console.group( "Recursion - external function." );
+				return( 3 );
+			}
+		)
+		.then(
+			function( n ) {
+				var tangentialPromiseBranch = recurseToZero( n,res);
+				return( tangentialPromiseBranch );
+
+			}
+		)
+		.then(
+			function(results) {
+
+				//
+				// results = _.uniqBy(results,'created_utc')
+				// console.log(items2.length);
+				// res.send(items2)
+				var red = {};
+				results.forEach(i =>{
+					if(i.type === 'comments'){
+						console.log(i);
+					}
+					!(itemsMap[i.subreddit]) ? itemsMap[i.subreddit] = [] : {};
+					red = {};
+					red = {
+						subreddit:i.subreddit,
+						title:i.title,
+						permalink:i.permalink,
+						type:i.type,
+						//link_title:i.link_title,
+						//link_permalink:i.link_permalink,
+						//body:i.body,
+						created_utc:i.created_utc,
+					}
+					itemsMap[i.subreddit].push(red);
+
+					//testing:
+					i.kind === 't1' ? comments.push({author:i.author,subreddit:i.subreddit,body:i.body}):{};
+				})
+
+
+				fs.writeFileSync('items.json', JSON.stringify(results));
+				//console.log(JSON.stringify(itemsMap));
+
+				var totals = {};
+				for (var prop in itemsMap){
+					!(totals[prop]) ? totals[prop] =0 : {};
+					totals[prop] = itemsMap[prop].length;
+					//todo: sort isn't working...
+					itemsMap[prop].sort(function(ll,rl){return ll.created_utc > rl.created_utc } )
+				}
+				fs.writeFileSync('itemsMap.json', JSON.stringify(itemsMap));
+
+				console.log(JSON.stringify(totals));
+
+				res.send(comments)
+				//res.send(itemsMap)
+
+				//console.groupEnd();
+
+			}
+		)
+})
+
+var token = "52995552-HTftCKFxCqOQJbSgFD-6nwc2G-Q";
 
 //recursion explained
 //https://www.bennadel.com/blog/3201-exploring-recursive-promises-in-javascript.htm
@@ -15,7 +98,63 @@ var options = {
 	json: true // Automatically stringifies the body to JSON
 };
 
-function start(){
+
+
+function makeOp(after){
+	var op = Object.assign({},options)
+	op.uri = op.uri + "&after=" + after;
+	console.log(op.uri);
+	return op;
+}
+
+var items = [];
+var itemsMap = {};
+
+function recurseToZero( after,res) {
+
+
+	console.log( "Entering recursive function for [", after, "]." );
+
+	// Once we hit zero, bail out of the recursion. The key to recursion is that
+	// it stops at some point, and the callstack can be "rolled" back up.
+
+	if(after === null || items.length === 2000){
+	//if(after === null){
+		console.log("finished",items.length);
+		return(items);
+
+	}
+
+	// Start a NEW PROMISE CHAIN that will become the continuation of the parent
+	// promise chain. This new promise chain now becomes a completely tangential
+	// branch to the parent promise chain.
+
+    var options = makeOp(after)
+	var tangentialPromiseBranch = rp(options).then(
+		function(body) {
+			console.log(body.data.children.length);
+
+			//testing:
+			//res.send(body.data);
+
+			body.data.children.forEach(i =>{
+				i.data.kind = i.kind;
+				items.push(i.data);
+			})
+			//console.log(JSON.stringify(items[0]));
+			//console.log(items[0].subreddit);
+			console.log("#items:",items.length);
+
+			//console.log(body.data.after);
+			return( recurseToZero( body.data.after ) ); // RECURSE!
+
+		})
+	return( tangentialPromiseBranch );
+
+}
+
+//depreciated
+function no_promise_recusion(){
 
 	var pages = [];
 //t1_g1e1r57
@@ -41,146 +180,4 @@ function start(){
 		.catch(function (err) {
 			console.log(err);
 		});
-}
-
-Promise.resolve()
-	.then(
-		function() {
-
-			console.group( "Recursion - external function." );
-			return( 3 );
-
-		}
-	)
-	.then(
-		function( n ) {
-
-			// With the recursive function factored out into its own stand-alone
-			// function, it's a lot easier to see that we are creating a totally
-			// TANGENTIAL BRANCH of the Promise chain.
-			// --
-			// A
-			// |
-			// B --> B'3 --> B'2 --> B'1 --> B'0
-			// |
-			// C
-			var tangentialPromiseBranch = recurseToZero( n );
-
-			return( tangentialPromiseBranch );
-
-		}
-	)
-	.then(
-		function(result) {
-			console.log(result.length);
-			console.log("-------------");
-			fs.writeFileSync('items.json', JSON.stringify(result));
-			//console.log(JSON.stringify(itemsMap));
-
-			var totals = {};
-			for (var prop in itemsMap){
-				!(totals[prop]) ? totals[prop] =0 : {};
-				totals[prop] = itemsMap[prop].length;
-				//todo: sort isn't working...
-				itemsMap[prop].sort(function(ll,rl){return ll.created_utc > rl.created_utc } )
-			}
-			fs.writeFileSync('itemsMap.json', JSON.stringify(itemsMap));
-			console.log(JSON.stringify(totals));
-			console.groupEnd();
-
-		}
-	)
-;
-
-
-function makeOp(after){
-	var op = Object.assign({},options)
-	op.uri = op.uri + "&after=" + after;
-	console.log(op.uri);
-	return op;
-}
-
-var items = [];
-var itemsMap = {};
-
-function recurseToZero( after ) {
-
-	console.log( "Entering recursive function for [", after, "]." );
-
-	// Once we hit zero, bail out of the recursion. The key to recursion is that
-	// it stops at some point, and the callstack can be "rolled" back up.
-	//if(after === null || after === 't3_hu6hgb'){
-	if(after === null){
-		console.log("finished",items.length);
-		return(items);
-
-	}
-
-	// Start a NEW PROMISE CHAIN that will become the continuation of the parent
-	// promise chain. This new promise chain now becomes a completely tangential
-	// branch to the parent promise chain.
-
-
-    var options = makeOp(after)
-	var tangentialPromiseBranch = rp(options).then(
-		function(body) {
-			console.log(body.data.children.length);
-			items = items.concat(body.data.children)
-			console.log("#items:",items.length);
-			items.forEach(i =>{
-				!(itemsMap[i.data.subreddit]) ? itemsMap[i.data.subreddit] = [] : {};
-				var red = {subreddit:i.data.subreddit,
-					title:i.data.title,
-					permalink:i.data.permalink,
-					//link_title:i.data.link_title,
-					//link_permalink:i.data.link_permalink,
-					//body:i.data.body,
-					created_utc:i.data.created_utc,
-
-				}
-				itemsMap[i.data.subreddit].push(red);
-			})
-			//console.log(body.data.after);
-			return( recurseToZero( body.data.after ) ); // RECURSE!
-
-		})
-
-	//var tangentialPromiseBranch = Promise.resolve().then(
-	//	function() {
-	//
-	//		return( recurseToZero( n - 1 ) ); // RECURSE!
-	//
-	//	}
-	//);
-
-	return( tangentialPromiseBranch );
-
-}
-
-// I am the recursive function, factored-out into its own function.
-function recurseToZero1( n ) {
-
-	console.log( "Entering recursive function for [", n, "]." );
-
-	// Once we hit zero, bail out of the recursion. The key to recursion is that
-	// it stops at some point, and the callstack can be "rolled" back up.
-	if ( n === 0 ) {
-
-		return( 0 );
-
-	}
-
-	// Start a NEW PROMISE CHAIN that will become the continuation of the parent
-	// promise chain. This new promise chain now becomes a completely tangential
-	// branch to the parent promise chain.
-	var tangentialPromiseBranch = Promise.resolve().then(
-		function() {
-
-			return( recurseToZero( n - 1 ) ); // RECURSE!
-
-		}
-	);
-
-	return( tangentialPromiseBranch );
-
 }
